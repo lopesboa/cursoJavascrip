d2045d075080df37a45102b0f7e98c9e59b1974c
Lopes Boa <lopesboa1@gmail.com>
2019-02-26 12:28:01 -0300

    Iniciando aplicaçao

---
+++ app-de-todos/app.js
@@ -0,0 +1,9 @@
+var listElemente = document.querySelector('app ul');
+var inputElemente = document.querySelector('app input');
+var buttonElemente = document.querySelector('app button');
+
+var todos = [
+    'Fazer café',
+    'Estudar Javascript',
+    'Acessar comunidade Rocketseat'
+]

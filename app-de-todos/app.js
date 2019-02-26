86fbfa445342ac425721a15d12a4a6a825c0cac8
Lopes Boa <lopesboa1@gmail.com>
2019-02-26 12:37:41 -0300

    Renderizando todos

--- app-de-todos/app.js
+++ app-de-todos/app.js
@@ -1,9 +1,22 @@
-var listElemente = document.querySelector('app ul');
-var inputElemente = document.querySelector('app input');
-var buttonElemente = document.querySelector('app button');
+var listElement = document.querySelector('#app ul');
+var inputElement = document.querySelector('#app input');
+var buttonElement = document.querySelector('#app button');
 
 var todos = [
     'Fazer café',
     'Estudar Javascript',
     'Acessar comunidade Rocketseat'
-]
+];
+
+function renderTodos() {
+    for (todo of todos){
+        var todoElement = document.createElement('li');
+        var todoText = document.createTextNode(todo);
+
+        todoElement.appendChild(todoText);
+        listElement.appendChild(todoElement);
+    }
+}
+
+renderTodos();
+
